3a578eba3ad889b64a2f7932aed564b7349a68e5
Mauricio <kakashi@Kakashi.Sensei>
2021-04-02 12:37:29 -0600

    drawcards method

--- components/Cards.vue
+++ components/Cards.vue
@@ -4,25 +4,8 @@
             The deck has been shuffled 
         </p>
         <p v-else> 
-            Shuffling deck 
+            Shuffling deck... 
         </p>
-        <ul>  
-            <li>
-                Ace of Hearts
-            </li>
-            <li>
-                Ace of Spades 
-            </li>
-            <li>
-                Jack of Diamonds 
-            </li>
-            <li>
-                10 of Clovers
-            </li>
-            <li>
-                4 of Spades
-            </li>
-        </ul>
     </div>    
 </template>
 
@@ -31,7 +14,8 @@
         name: "Cards",
         data() { 
             return {
-                deck: null
+                deck: null,
+                drawards: []
             } 
         },
         mounted() {
@@ -41,9 +25,15 @@
             getDeck() {
                 let uri = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
                 this.$axios.get(uri).then(response => {
-                    this.deck = response.data
+                    this.deck = response.data;
+                    this.drawCards(2, this.deck.deck_id)
                 }).catch(() => { alert('oops: error!') });
-
+            },
+            drawCards(numCards, deckId) {
+                let uri = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numCards}`;
+                this.$axios.get(uri).then(response => {
+                    console.log(response.data)
+                }).catch(() => { alert('oops: error!') });
             }
         }
     }
